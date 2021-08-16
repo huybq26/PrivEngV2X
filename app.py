51c5a378058a9b2707c06823bfa162b87442d3c0
Quang Huy <81632425+huybq26@users.noreply.github.com>
2021-08-17 01:35:42 +0800

    Improve performance of live demo

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from generate_path import G_pro
 import threading
 import osmnx as ox
 from matplotlib import animation
@@ -18,6 +19,71 @@ from matplotlib.figure import Figure
 
 # from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 # from matplotlib.figure import Figure
+
+
+def read_txt_column(data_name, f_row='delete'):
+    data = open(data_name)
+    result = []
+    for line in data.readlines():
+        a = line.split(',')
+        result.append(a)
+    if f_row == 'delete':
+        del result[0]
+    result_array = np.array(result, dtype=float)
+    return result_array
+
+# global G_pro, fig, ax
+
+
+def run_map_init():
+
+    print("Loading map!")
+
+    # draw a figure of trajectory with map
+    # ---------------------------------------------------------------------------------------
+    # draw_map
+    global G_pro
+    global fig
+    global ax
+    G_pro = joblib.load('NTU_project_map_filled.pkl')
+    fig, ax = ox.plot_graph(G_pro, show=False, close=False,
+                            node_color='#16d2d9', bgcolor="#ffffff")
+
+    print("Loaded map!")
+    # draw_trajectory
+
+
+def run_path_live():
+    # read data
+    # ---------------------------------------------------------------------------------------
+    bsm_file = "./output.txt"
+    sanitized_bsm_file = "./x_y_bsm_sanitized_test.txt"
+    data_bsm = read_txt_column(bsm_file)
+    sanitized_bsm = read_txt_column(sanitized_bsm_file)
+
+    x = data_bsm[:, 0]
+    y = data_bsm[:, 1]
+    s_x = sanitized_bsm[:, 0]
+    s_y = sanitized_bsm[:, 1]
+    # ax.scatter(s_x[0], s_y[0], s=70, label='EKF Start', c='y', marker='o')
+    # ax.scatter(x[0], y[0], s=70, label='Start', c='g', marker='*')
+
+    plt.plot(s_x[::5], s_y[::5], '+', mfc='none',
+             label='EKF Position', c='k', markersize=5)
+    plt.plot(x[::10], y[::10], 'o', mfc='none',
+             label='GPS Measurements', c='r', markersize=5)
+
+    # plt.plot(s_x[-1], s_y[-1], 's', label='EKF Goal', c='r', markersize=10)
+    # plt.plot(x[-1], y[-1], 'X',  label='Goal', c='b', markersize=10)
+    plt.plot(s_x[-1], s_y[-1], 's',  c='r', markersize=10)
+    plt.plot(x[-1], y[-1], 'X',   c='b', markersize=10)
+    plt.xlabel('X [m]')
+    plt.ylabel('Y [m]')
+    plt.legend(loc='best')
+    plt.axis('equal')
+    plt.savefig("./static/live.jpg", dpi=200)
+    # plt.show()
+
 
 app = Flask(__name__)
 # app.config['UPLOAD_EXTENSIONS'] = ['.txt']
@@ -49,10 +115,19 @@ def input():
     return render_template("input.html")
 
 
+count = 0
+
+
 @app.route('/live.html', methods=["GET", "POST"])
 @app.route('/live', methods=["GET", "POST"])
 def live():
-    runpy.run_path(path_name='generate_path_live.py')
+    global count
+    if count == 0:
+        run_map_init()
+        count = count + 1
+    run_path_live()
+    # runpy.run_path(path_name='generate_path_live.py')
+
     return render_template("live.html")
 
 
@@ -113,6 +188,6 @@ if __name__ == "__main__":
     from livereload import Server
     server = Server(app.wsgi_app)
     # server.watch('x_y_bsm_sanitized_test.txt')
-    server.watch('x_y_bsm_test.txt')
+    server.watch('output.txt')
     server.watch('x_y_bsm_sanitized_test.txt')
-    server.serve()
+    server.serve(open_url_delay=0, debug=False)
